b9d4197685248f62dada554c962c923f534e60d6
Jesu <Jesu@JESU>
2017-04-05 18:56:22 +0200

    renombrado FrmConferencias

--- Practica 3/Presentación/FrmConferencias.Designer.vb
+++ Practica 3/Presentación/FrmConferencias.Designer.vb
@@ -23,43 +23,43 @@ Partial Class FrmConferencias
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnAñadir = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.txtbxFecha_fin = New System.Windows.Forms.TextBox()
         Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox6 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtbxLugar = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtbxSiglas = New System.Windows.Forms.TextBox()
+        Me.txtbxIDConferencia = New System.Windows.Forms.TextBox()
+        Me.lblLugar = New System.Windows.Forms.Label()
+        Me.lblFecha_fin = New System.Windows.Forms.Label()
+        Me.lblFecha_inicio = New System.Windows.Forms.Label()
+        Me.lblNombreConferencia = New System.Windows.Forms.Label()
+        Me.lblSiglas = New System.Windows.Forms.Label()
+        Me.lblIDConferencia = New System.Windows.Forms.Label()
+        Me.lstbxConferencias = New System.Windows.Forms.ListBox()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.Button3)
-        Me.GroupBox1.Controls.Add(Me.Button2)
-        Me.GroupBox1.Controls.Add(Me.Button1)
-        Me.GroupBox1.Controls.Add(Me.TextBox6)
+        Me.GroupBox1.Controls.Add(Me.btnModificar)
+        Me.GroupBox1.Controls.Add(Me.btnAñadir)
+        Me.GroupBox1.Controls.Add(Me.btnEliminar)
+        Me.GroupBox1.Controls.Add(Me.txtbxFecha_fin)
         Me.GroupBox1.Controls.Add(Me.TextBox5)
-        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.txtbxLugar)
         Me.GroupBox1.Controls.Add(Me.TextBox3)
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
-        Me.GroupBox1.Controls.Add(Me.Label6)
-        Me.GroupBox1.Controls.Add(Me.Label5)
-        Me.GroupBox1.Controls.Add(Me.Label4)
-        Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.txtbxSiglas)
+        Me.GroupBox1.Controls.Add(Me.txtbxIDConferencia)
+        Me.GroupBox1.Controls.Add(Me.lblLugar)
+        Me.GroupBox1.Controls.Add(Me.lblFecha_fin)
+        Me.GroupBox1.Controls.Add(Me.lblFecha_inicio)
+        Me.GroupBox1.Controls.Add(Me.lblNombreConferencia)
+        Me.GroupBox1.Controls.Add(Me.lblSiglas)
+        Me.GroupBox1.Controls.Add(Me.lblIDConferencia)
+        Me.GroupBox1.Controls.Add(Me.lstbxConferencias)
         Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.GroupBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight
         Me.GroupBox1.Location = New System.Drawing.Point(31, 43)
@@ -69,108 +69,42 @@ Partial Class FrmConferencias
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "CONFERENCIAS"
         '
-        'ListBox1
+        'btnModificar
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 16
-        Me.ListBox1.Location = New System.Drawing.Point(42, 40)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(203, 420)
-        Me.ListBox1.TabIndex = 0
+        Me.btnModificar.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnModificar.Location = New System.Drawing.Point(553, 362)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(147, 64)
+        Me.btnModificar.TabIndex = 15
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
         '
-        'Label1
+        'btnAñadir
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label1.Location = New System.Drawing.Point(364, 106)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(31, 20)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "ID:"
+        Me.btnAñadir.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnAñadir.Location = New System.Drawing.Point(351, 362)
+        Me.btnAñadir.Name = "btnAñadir"
+        Me.btnAñadir.Size = New System.Drawing.Size(147, 64)
+        Me.btnAñadir.TabIndex = 14
+        Me.btnAñadir.Text = "Añadir"
+        Me.btnAñadir.UseVisualStyleBackColor = True
         '
-        'Label2
+        'btnEliminar
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label2.Location = New System.Drawing.Point(364, 177)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(60, 20)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Siglas:"
+        Me.btnEliminar.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnEliminar.Location = New System.Drawing.Point(757, 362)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(147, 64)
+        Me.btnEliminar.TabIndex = 13
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
         '
-        'Label3
+        'txtbxFecha_fin
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label3.Location = New System.Drawing.Point(364, 247)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(73, 20)
-        Me.Label3.TabIndex = 3
-        Me.Label3.Text = "Nombre:"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label4.Location = New System.Drawing.Point(643, 177)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(108, 20)
-        Me.Label4.TabIndex = 4
-        Me.Label4.Text = "Fecha_inicio:"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label5.Location = New System.Drawing.Point(643, 247)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(82, 20)
-        Me.Label5.TabIndex = 5
-        Me.Label5.Text = "Ficha_fin:"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Label6.Location = New System.Drawing.Point(643, 106)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(57, 20)
-        Me.Label6.TabIndex = 6
-        Me.Label6.Text = "Lugar:"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(441, 103)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
-        Me.TextBox1.TabIndex = 7
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(441, 175)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(132, 22)
-        Me.TextBox2.TabIndex = 8
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(441, 245)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(132, 22)
-        Me.TextBox3.TabIndex = 9
-        '
-        'TextBox4
-        '
-        Me.TextBox4.Location = New System.Drawing.Point(756, 103)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(136, 22)
-        Me.TextBox4.TabIndex = 10
+        Me.txtbxFecha_fin.Location = New System.Drawing.Point(756, 244)
+        Me.txtbxFecha_fin.Name = "txtbxFecha_fin"
+        Me.txtbxFecha_fin.Size = New System.Drawing.Size(136, 22)
+        Me.txtbxFecha_fin.TabIndex = 12
         '
         'TextBox5
         '
@@ -179,42 +113,108 @@ Partial Class FrmConferencias
         Me.TextBox5.Size = New System.Drawing.Size(135, 22)
         Me.TextBox5.TabIndex = 11
         '
-        'TextBox6
+        'txtbxLugar
         '
-        Me.TextBox6.Location = New System.Drawing.Point(756, 244)
-        Me.TextBox6.Name = "TextBox6"
-        Me.TextBox6.Size = New System.Drawing.Size(136, 22)
-        Me.TextBox6.TabIndex = 12
+        Me.txtbxLugar.Location = New System.Drawing.Point(756, 103)
+        Me.txtbxLugar.Name = "txtbxLugar"
+        Me.txtbxLugar.Size = New System.Drawing.Size(136, 22)
+        Me.txtbxLugar.TabIndex = 10
         '
-        'Button1
+        'TextBox3
         '
-        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Button1.Location = New System.Drawing.Point(757, 362)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(147, 64)
-        Me.Button1.TabIndex = 13
-        Me.Button1.Text = "Eliminar"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.TextBox3.Location = New System.Drawing.Point(441, 245)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox3.TabIndex = 9
         '
-        'Button2
+        'txtbxSiglas
         '
-        Me.Button2.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Button2.Location = New System.Drawing.Point(351, 362)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(147, 64)
-        Me.Button2.TabIndex = 14
-        Me.Button2.Text = "Añadir"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.txtbxSiglas.Location = New System.Drawing.Point(441, 175)
+        Me.txtbxSiglas.Name = "txtbxSiglas"
+        Me.txtbxSiglas.Size = New System.Drawing.Size(132, 22)
+        Me.txtbxSiglas.TabIndex = 8
         '
-        'Button3
+        'txtbxIDConferencia
         '
-        Me.Button3.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Button3.Location = New System.Drawing.Point(553, 362)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(147, 64)
-        Me.Button3.TabIndex = 15
-        Me.Button3.Text = "Modificar"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.txtbxIDConferencia.Location = New System.Drawing.Point(441, 103)
+        Me.txtbxIDConferencia.Name = "txtbxIDConferencia"
+        Me.txtbxIDConferencia.Size = New System.Drawing.Size(132, 22)
+        Me.txtbxIDConferencia.TabIndex = 7
+        '
+        'lblLugar
+        '
+        Me.lblLugar.AutoSize = True
+        Me.lblLugar.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLugar.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblLugar.Location = New System.Drawing.Point(643, 106)
+        Me.lblLugar.Name = "lblLugar"
+        Me.lblLugar.Size = New System.Drawing.Size(57, 20)
+        Me.lblLugar.TabIndex = 6
+        Me.lblLugar.Text = "Lugar:"
+        '
+        'lblFecha_fin
+        '
+        Me.lblFecha_fin.AutoSize = True
+        Me.lblFecha_fin.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFecha_fin.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblFecha_fin.Location = New System.Drawing.Point(643, 247)
+        Me.lblFecha_fin.Name = "lblFecha_fin"
+        Me.lblFecha_fin.Size = New System.Drawing.Size(87, 20)
+        Me.lblFecha_fin.TabIndex = 5
+        Me.lblFecha_fin.Text = "Fecha_fin:"
+        '
+        'lblFecha_inicio
+        '
+        Me.lblFecha_inicio.AutoSize = True
+        Me.lblFecha_inicio.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFecha_inicio.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblFecha_inicio.Location = New System.Drawing.Point(643, 177)
+        Me.lblFecha_inicio.Name = "lblFecha_inicio"
+        Me.lblFecha_inicio.Size = New System.Drawing.Size(108, 20)
+        Me.lblFecha_inicio.TabIndex = 4
+        Me.lblFecha_inicio.Text = "Fecha_inicio:"
+        '
+        'lblNombreConferencia
+        '
+        Me.lblNombreConferencia.AutoSize = True
+        Me.lblNombreConferencia.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNombreConferencia.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblNombreConferencia.Location = New System.Drawing.Point(364, 247)
+        Me.lblNombreConferencia.Name = "lblNombreConferencia"
+        Me.lblNombreConferencia.Size = New System.Drawing.Size(73, 20)
+        Me.lblNombreConferencia.TabIndex = 3
+        Me.lblNombreConferencia.Text = "Nombre:"
+        '
+        'lblSiglas
+        '
+        Me.lblSiglas.AutoSize = True
+        Me.lblSiglas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSiglas.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblSiglas.Location = New System.Drawing.Point(364, 177)
+        Me.lblSiglas.Name = "lblSiglas"
+        Me.lblSiglas.Size = New System.Drawing.Size(60, 20)
+        Me.lblSiglas.TabIndex = 2
+        Me.lblSiglas.Text = "Siglas:"
+        '
+        'lblIDConferencia
+        '
+        Me.lblIDConferencia.AutoSize = True
+        Me.lblIDConferencia.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblIDConferencia.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblIDConferencia.Location = New System.Drawing.Point(364, 106)
+        Me.lblIDConferencia.Name = "lblIDConferencia"
+        Me.lblIDConferencia.Size = New System.Drawing.Size(31, 20)
+        Me.lblIDConferencia.TabIndex = 1
+        Me.lblIDConferencia.Text = "ID:"
+        '
+        'lstbxConferencias
+        '
+        Me.lstbxConferencias.FormattingEnabled = True
+        Me.lstbxConferencias.ItemHeight = 16
+        Me.lstbxConferencias.Location = New System.Drawing.Point(42, 40)
+        Me.lstbxConferencias.Name = "lstbxConferencias"
+        Me.lstbxConferencias.Size = New System.Drawing.Size(203, 420)
+        Me.lstbxConferencias.TabIndex = 0
         '
         'FrmConferencias
         '
@@ -231,20 +231,20 @@ Partial Class FrmConferencias
     End Sub
 
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents txtbxLugar As TextBox
     Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label6 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label1 As Label
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents txtbxSiglas As TextBox
+    Friend WithEvents txtbxIDConferencia As TextBox
+    Friend WithEvents lblLugar As Label
+    Friend WithEvents lblFecha_fin As Label
+    Friend WithEvents lblFecha_inicio As Label
+    Friend WithEvents lblNombreConferencia As Label
+    Friend WithEvents lblSiglas As Label
+    Friend WithEvents lblIDConferencia As Label
+    Friend WithEvents lstbxConferencias As ListBox
+    Friend WithEvents txtbxFecha_fin As TextBox
     Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnAñadir As Button
+    Friend WithEvents btnEliminar As Button
 End Class
