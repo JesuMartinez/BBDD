d876d204cb9793aa8555a7238b00534d5758bc1b
alber <albertovinaroz@live.com>
2017-04-05 17:13:00 +0200

    fromluario

--- Practica 3/Presentación/FrmConferencias.vb
+++ Practica 3/Presentación/FrmConferencias.vb
@@ -39,41 +39,35 @@
 
     Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
         Dim con As New Conferencia(Convert.ToInt16(lstbxConferencias.SelectedItem.ToString))
-        If MessageBox.Show("¿Desea eliminar la conferencia con ID: " + con.IDConferencia + " de la base de datos?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+        If MessageBox.Show("¿Desea eliminar la conferencia con ID: " + con.IDConferencia.ToString + " de la base de datos?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
             Try
-                per.deletePersona()
-                listbxPersonas.Items.Remove(listbxPersonas.SelectedItem)
+                con.deleteConferencia()
+                lstbxConferencias.Items.Remove(lstbxConferencias.SelectedItem)
             Catch ex As Exception
                 MessageBox.Show(ex.Message)
                 Exit Sub
             End Try
-            txtbxDNI.Clear()
-            txtbxNombre.Clear()
+            txtbxIDConferencia.Clear()
+            txtbxNombreConferencia.Clear()
         End If
     End Sub
 
-    Private Sub btnLimpiarCampos_Click(sender As Object, e As EventArgs) Handles btnLimpiarCampos.Click
-        txtbxDNI.Clear()
-        txtbxNombre.Clear()
-        listbxPersonas.ClearSelected()
-    End Sub
 
     Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
-        Dim per As Persona
-        If (txtbxDNI.Modified = False And txtbxNombre.Modified = True) Then
-            per = New Persona(txtbxDNI.Text)
-            per.Nombre = txtbxNombre.Text
-            If MessageBox.Show("¿Desea modificar al usuario seleccionado?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+        Dim con As Conferencia
+        If (txtbxIDConferencia.Modified = False And txtbxNombreConferencia.Modified = True) Then
+            con = New Conferencia(Convert.ToInt16(txtbxIDConferencia.Text))
+            con.Nombre = txtbxNombreConferencia.Text
+            If MessageBox.Show("¿Desea modificar la conferencia seleccionado?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
                 Try
-                    per.updatePersona()
-                    MessageBox.Show("El usuario ha sido modificado correctamente.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    con.updateConferencia()
                 Catch ex As Exception
                     MessageBox.Show(ex.Message)
                     Exit Sub
                 End Try
             End If
         Else
-            MessageBox.Show("Modificación de DNI inválida ya que corresponde con la clave primaria de nuestra base de datos o modificación del nombre de usuario no permitida al tratarse del mismo. " & vbNewLine & "Por favor modifique únicamente el nombre de usuario.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            MessageBox.Show("Modificación de ID inválida ya que corresponde con la clave primaria de nuestra base de datos o modificación del nombre de conferencia no permitida al tratarse de la misma. " & vbNewLine & "Por favor modifique únicamente el nombre de la conferencia.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information)
         End If
     End Sub
 End Class
