e516b5860c3044d43e3480b70681db0dd9b135c7
alber <albertovinaroz@live.com>
2017-05-01 17:48:37 +0200

    groupbox

--- Practica 3/Presentación/FrmAsistencias.Designer.vb
+++ Practica 3/Presentación/FrmAsistencias.Designer.vb
@@ -29,13 +29,19 @@ Partial Class FrmAsistencias
         Me.btnAplicarGuardar = New System.Windows.Forms.Button()
         Me.lstbxInvestigadores = New System.Windows.Forms.ListBox()
         Me.btnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'ListbxConferenciasAsiste
         '
         Me.ListbxConferenciasAsiste.FormattingEnabled = True
         Me.ListbxConferenciasAsiste.ItemHeight = 16
-        Me.ListbxConferenciasAsiste.Location = New System.Drawing.Point(877, 40)
+        Me.ListbxConferenciasAsiste.Location = New System.Drawing.Point(17, 28)
         Me.ListbxConferenciasAsiste.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.ListbxConferenciasAsiste.Name = "ListbxConferenciasAsiste"
         Me.ListbxConferenciasAsiste.Size = New System.Drawing.Size(212, 404)
@@ -45,7 +51,7 @@ Partial Class FrmAsistencias
         '
         Me.ListbxConferencias.FormattingEnabled = True
         Me.ListbxConferencias.ItemHeight = 16
-        Me.ListbxConferencias.Location = New System.Drawing.Point(400, 40)
+        Me.ListbxConferencias.Location = New System.Drawing.Point(15, 28)
         Me.ListbxConferencias.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.ListbxConferencias.Name = "ListbxConferencias"
         Me.ListbxConferencias.Size = New System.Drawing.Size(212, 404)
@@ -88,7 +94,7 @@ Partial Class FrmAsistencias
         '
         Me.lstbxInvestigadores.FormattingEnabled = True
         Me.lstbxInvestigadores.ItemHeight = 16
-        Me.lstbxInvestigadores.Location = New System.Drawing.Point(61, 40)
+        Me.lstbxInvestigadores.Location = New System.Drawing.Point(20, 32)
         Me.lstbxInvestigadores.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.lstbxInvestigadores.Name = "lstbxInvestigadores"
         Me.lstbxInvestigadores.Size = New System.Drawing.Size(212, 404)
@@ -103,22 +109,58 @@ Partial Class FrmAsistencias
         Me.btnSalir.Text = "Salir"
         Me.btnSalir.UseVisualStyleBackColor = True
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListbxConferencias)
+        Me.GroupBox1.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.GroupBox1.Location = New System.Drawing.Point(388, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(243, 450)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Lista de conferencias totales"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lstbxInvestigadores)
+        Me.GroupBox2.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.GroupBox2.Location = New System.Drawing.Point(41, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(252, 449)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Lista de investigadores"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ListbxConferenciasAsiste)
+        Me.GroupBox3.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.GroupBox3.Location = New System.Drawing.Point(867, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(244, 448)
+        Me.GroupBox3.TabIndex = 9
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Conferencias a las que asiste"
+        '
         'FrmAsistencias
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1177, 539)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.btnSalir)
-        Me.Controls.Add(Me.lstbxInvestigadores)
         Me.Controls.Add(Me.btnAplicarGuardar)
         Me.Controls.Add(Me.Eliminar)
         Me.Controls.Add(Me.Añadir)
-        Me.Controls.Add(Me.ListbxConferencias)
-        Me.Controls.Add(Me.ListbxConferenciasAsiste)
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "FrmAsistencias"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Asistencias"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -130,4 +172,7 @@ Partial Class FrmAsistencias
     Friend WithEvents btnAplicarGuardar As Button
     Friend WithEvents lstbxInvestigadores As ListBox
     Friend WithEvents btnSalir As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
 End Class
